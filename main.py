146ab638ad480217defb96ef012c0b786ba6df64
vivimix17 <animevivi14@gmail.com>
2023-11-22 19:51:25 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#estrutura condicional
+#idade = int(input('digite sua idade'))
+
+#if(idade >= 18):
+    #print('você pode tirar a carteira de motorista')
+#else:
+    #print('espere algum tempo sem carteira')
+
+
+ #operadores relacionais
+ #maior que >
+ #menor que <
+ #maior igaul >=
+ #menor igual <=
+ #igual==
+ #diferente!=
+
+ nome = input('digite seu nome')
+
+ if(nome == 'Bastião'):
+     print('você é o amigo do coração de Rian')
+else:
+     print('você deveria ser')
